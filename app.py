2b729a9e426e4b2d01a880f1e20313db2dbd4976
anindyaTHEgrt <anindyasanjeev24@gmail.com>
2024-11-03 16:29:35 +0530

    graph with map overlay done.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 import random
 import matplotlib.pyplot as plt
+import matplotlib.image as image
 
 app = Flask(__name__, template_folder='template')
 
@@ -20,17 +21,10 @@ taxis = []
 def generate_passenger_locations(num_passengers):
     passengers = []
     for _ in range(num_passengers):
-        lat = random.uniform(40.0, 41.0)  # Example latitude range
-        lon = random.uniform(-74.0, -73.0)  # Example longitude range
+        lon = random.uniform(40.0, 41.0)  # Example latitude range
+        lat = random.uniform(-74.0, -73.0)  # Example longitude range
         passengers.append([lat, lon])
     return np.array(passengers)
-
-# Generate random taxi locations (latitude, longitude)
-'''def generate_taxi_locations(num_taxis):
-    taxis = []
-    taxis.append([CorLat, CorLon]) #error here, dimension error
-    print(taxis)
-    return np.array(taxis)'''
 
 # Assign passengers to taxis based on nearest taxi location
 def assign_passengers_to_taxis(taxi_locations, passenger_locations):
@@ -69,6 +63,9 @@ def submit_coordinates():
 def start_function():
     num_passengers = 50
 
+    im = image.imread('assets/barca_map.png')
+    fig, ax = plt.subplots()
+
 
     # Generate random passenger and taxi locations
     passenger_locations = generate_passenger_locations(num_passengers)
@@ -80,9 +77,9 @@ def start_function():
     kmeans = assign_passengers_to_taxis(taxi_locations, passenger_locations)
 
     # Visualization of passengers, taxis, and assignments
-    plt.scatter(passenger_locations[:, 0], passenger_locations[:, 1], c=kmeans.labels_, s=50, cmap='rainbow',
-                label='Passengers')
-    plt.scatter(taxi_locations[:, 0], taxi_locations[:, 1], s=200, c='black', marker='X', label='Taxis')
+    ax.scatter(passenger_locations[:, 0], passenger_locations[:, 1], c=kmeans.labels_, s=50, cmap='rainbow',
+                label='Passengers', zorder=2)
+    ax.scatter(taxi_locations[:, 0], taxi_locations[:, 1], s=200, c='black', marker='X', label='Taxis', zorder=2)
 
     # Annotating taxi locations
     for i, (x, y) in enumerate(taxi_locations):
@@ -91,9 +88,11 @@ def start_function():
     for i, (x, y) in enumerate(passenger_locations):
         plt.text(x, y, f'P {i}', fontsize=5)
 
+    fig.figimage(im, 0, 0, zorder=1, alpha=.3)
+
     plt.title('Passenger Assignment to Nearest Taxi Using K-Means')
-    plt.xlabel('Latitude')
-    plt.ylabel('Longitude')
+    plt.xlabel('Latitude', zorder=2)
+    plt.ylabel('Longitude', zorder=2)
     plt.legend()
     plt.show()
 
